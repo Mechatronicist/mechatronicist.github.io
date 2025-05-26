c06de6333b4dcc972ecf008c68bf12b9264834c7
lewpar <lewis.o.parmenter@gmail.com>
2025-05-26 20:03:59 +1000

    Removed default margin from page and cleaned up window components folder structure.

--- src/lib/programs.ts
+++ src/lib/programs.ts
@@ -1,17 +1,10 @@
 import { shallowRef } from "vue";
-import HelloWorld from "../components/HelloWorld.vue";
-import UuidGenerator from "../components/UuidGenerator.vue";
-import Terminal from "../components/Terminal.vue";
+
 import { registerWindow } from "./windows";
-import About from "../components/About.vue";
+import About from "../components/windows/About.vue";
+import Terminal from "../components/windows/Terminal.vue";
 
 export function registerPrograms() {
-    registerWindow({
-        id: 'uuid-gen',
-        name: "UUID Generator",
-        component: shallowRef(UuidGenerator)
-    });
-
     registerWindow({
         id: 'about',
         name: "About",
@@ -26,11 +19,5 @@ export function registerPrograms() {
             x: 600,
             y: 380
         }
-    });
-
-    registerWindow({
-        id: "test",
-        name: "Test Window",
-        component: shallowRef(HelloWorld)
     });
 }
