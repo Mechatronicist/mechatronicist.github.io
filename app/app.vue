cc16607e231a212fbb71a930bdf280eb97c4b92e
lewpar <lewis.o.parmenter@gmail.com>
2025-10-09 18:03:43 +1100

    Implemented profile.

--- app/app.vue
+++ app/app.vue
@@ -6,6 +6,18 @@
 
 <style>
 body {
+	color: white;
 	background-color: #101010;
+
+	overflow-x: hidden;
+}
+
+.box {
+	border: 1px solid #202020;
+	border-radius: 5px;
+
+	padding: 1rem;
+
+	background-color: #151515;
 }
 </style>
--- app/components/Profile.vue
+++ app/components/Profile.vue
@@ -16,5 +16,38 @@ const Profile = ref<Profile>(ProfileData);
 </script>
 
 <template>
-
+    <div id="profile" class="box">
+        <div id="name">{{ Profile.name }}</div>
+        <div id="description">{{ Profile.description }}</div>
+        <div id="socials">
+            <h2>Socials</h2>
+            <a v-for="social in Profile.socials" :href="`${social.link}`">{{ social.name }}</a>
+        </div>
+    </div>
 </template>
+
+<style scoped>
+#profile #name {
+    font-weight: bold;
+    font-size: 1.25rem;
+}
+#profile #socials {
+    display: flex;
+    flex-direction: column;
+
+    gap: 0.25rem;
+}
+#profile #socials h2 {
+    font-weight: bold;
+    font-size: 1.1rem;
+}
+#profile #socials a {
+    color: dodgerblue;
+    text-decoration: none;
+
+    transition: color 0.25s;
+}
+#profile #socials a:hover {
+    color: rgb(12, 110, 208);
+}
+</style>
