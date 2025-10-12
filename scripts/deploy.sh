692f0b3058e1909836009d0a071adb5218fade97
lewpar <lewis.o.parmenter@gmail.com>
2025-10-12 12:27:17 +1100

    Update GH Pages build.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+# Generate static website
 rm -fr ./docs
 npm run generate
-cp -r .output/public ./docs
+npx ipx generate public ./.output/public/_ipx
+cp -r ./.output/public ./docs
+
+# Disable GitHub Jekyll
 touch ./docs/.nojekyll
+
+# Commit and Push changes
 git add ./
 git commit -am "Update GH Pages build."
 git push
